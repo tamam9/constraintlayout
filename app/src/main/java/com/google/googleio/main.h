007274eea224dda36c8704dc0a050102e7bdca96
yusufolu <383606116@qq.com>
2016-09-23 16:25:20 +0800

    constraintLayout first

---
+++ app/src/main/java/com/google/googleio/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Yusuf on 2016/9/23.
+//
+
+#ifndef CONSTRAINT_LAYOUT_START_MAIN_H
+#define CONSTRAINT_LAYOUT_START_MAIN_H
+
+#endif //CONSTRAINT_LAYOUT_START_MAIN_H
+
+public class Test{
+
+public int num;
+
+}
---
+++ app/src/main/java/com/google/googleio/sour.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Yusuf on 2016/9/23.
+//
+
